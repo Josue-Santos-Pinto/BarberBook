0e7bd6b1a0e3460e6a44e9943428c2be491faf27
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-02-24 16:29:49 -0300

    Svgs Added && Preload screen completed

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, { useEffect } from 'react';
+import { Container, LoadingIcon } from './styles';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import BarberLogo from '../../assets/barber.svg';
+import { useNavigation } from '@react-navigation/native';
 
 export default () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token !== null) {
+        // Validar Token
+      } else {
+        navigation.reset({
+          index: 1,
+          routes: [{ name: 'SignIn' }],
+        });
+      }
+    };
+    checkToken();
+  }, []);
+
   return (
     <Container>
-      <Text>Preload</Text>
+      <BarberLogo width="100%" height="160" />
+      <LoadingIcon size="large" color="#FFF" />
     </Container>
   );
 };
