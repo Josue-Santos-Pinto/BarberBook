fbba4d057ba93e130a5a821467366a387ecd9639
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-02-27 12:05:53 -0300

    project name updated

--- ios/DevBarberTests/DevBarberTests.m
+++ ios/DevBarberTests/DevBarberTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyBandTests : XCTestCase
+@interface DevBarberTests : XCTestCase
 
 @end
 
-@implementation MyBandTests
+@implementation DevBarberTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
