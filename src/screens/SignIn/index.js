f20875426dd5d34c71ad42441f8598968227a8b0
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-02-27 10:28:19 -0300

    signin && signup screen created

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,23 +1,62 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container, InputArea } from './styles';
+import React, { useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import SignInput from '../../components/SignInput';
+
 import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  const handleSignClick = () => {};
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      index: 1,
+      routes: [{ name: 'SignUp' }],
+    });
+  };
+
   return (
     <Container>
       <BarberLogo width="100%" height="160" />
 
       <InputArea>
-        <SignInput />
-        <SignInput />
-
-        <CustomButton>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={(t) => setEmailField(t)}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          value={passwordField}
+          onChangeText={(t) => setPasswordField(t)}
+          password={true}
+        />
+        <CustomButton onPress={handleSignClick}>
           <CustomButtonText>LOGIN</CustomButtonText>
         </CustomButton>
       </InputArea>
 
-      <SignMessageButton>
+      <SignMessageButton onPress={handleMessageButtonClick}>
         <SignMessageButtonText>Ainda não possui uma conta ?</SignMessageButtonText>
         <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
       </SignMessageButton>
